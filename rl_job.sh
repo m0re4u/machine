4a4f8a07b3fbed2c27e788a4b26e2594403d6267
Michiel van der Meer <michielmeer@live.nl>
2019-02-18 18:10:59 +0100

    Removed unnecessary module loads

--- rl_job.sh
+++ rl_job.sh
@@ -3,11 +3,9 @@
 #SBATCH -p gpu_short
 #SBATCH -t 00:30:00
 
-set -x
-
 #Load the modules needed for your environment
 module load Python/3.6.3-foss-2017b
-module load CUDA/9.0.176 cuDNN/7.4.1.5-CUDA-9.0.176 NCCL/2.2.12-CUDA-9.0.176
+module load CUDA/10.0.130
 
 pushd ${HOME}/machine/
 python3 train_rl.py --env-name BabyAI-GoToLocal-v0 --print_every 1
