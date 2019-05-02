f73e5d9f9a19fe088f436acbde4ec0417b6d4b89
Michiel van der Meer <michiel@vandermeer.ai>
2019-05-02 15:33:38 +0200

    Added transfer jobs

---
+++ jobs/transfer/transfer_AC_PutNextLocal_segment.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -p gpu_shared
+#SBATCH -t 16:00:00
+#SBATCH --mem=12GB
+
+#SBATCH --mail-type=END
+#SBATCH --mail-user=michiel.vandermeer@student.uva.nl
+
+#Load the modules needed for your environment
+module load Python/3.6.3-foss-2017b
+module load CUDA/9.0.176
+
+# Go to machine directory
+pushd ${HOME}/machine/
+
+# Run training
+python3 train_rl.py \
+    --env-name BabyAI-PutNextLocal-v0 \
+    --print_every 1 \
+    --segment_level segment \
+    --seed 1 \
+    --slurm_id $SLURM_JOB_ID \
+    --resume \
+    --load_checkpoint models/BabyAI-GoToLocal-v0-_PPO_AC_expert_filmcnn_gru_mem_seed1_job2238716_19-05-01-13-29-03/009200_check.pt \
+    --tb
