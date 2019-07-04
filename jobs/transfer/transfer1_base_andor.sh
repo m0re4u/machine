5565b046ff987da9cc71b81b39b9a22df998966b
Michiel van der Meer <michiel@vandermeer.ai>
2019-07-04 15:51:56 +0200

    AndOR transfer jobs

---
+++ jobs/transfer/transfer1_base_andor.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -p gpu_shared
+#SBATCH -t 1:00:00
+#SBATCH --mem=12GB
+
+#SBATCH --mail-type=END
+#SBATCH --mail-user=michiel.vandermeer@student.uva.nl
+
+#Load the modules needed for your environment
+module load Python/3.6.3-foss-2017b
+module load CUDA/9.0.176
+
+# Go to machine directory
+pushd ${HOME}/machine/
+
+# Run training
+python3 train_rl.py \
+    --env-name BabyAI-TransferGoToObjAndOr1-v0 \
+    --print_every 1 \
+    --slurm_id $SLURM_JOB_ID \
+    --tb \
+    --seed 1 \
+    --load_checkpoint models/BabyAI-CustomGoToObjAndOr-v0-_PPO_AC_expert_filmcnn_gru_mem_seed42_job2607635_19-06-27-11-22-26/006900_check.pt \
+    --resume
+
