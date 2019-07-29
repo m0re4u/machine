43d9cf3888af48ab9658db9555f0fd0e4780deee
Michiel van der Meer <michiel@vandermeer.ai>
2019-07-29 17:01:15 +0200

    Added inverted diag beforeafter

---
+++ jobs/transfer/beforeafter/diag_transfer0_base_beforeafter100.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -p gpu_shared
+#SBATCH -t 1:00:00
+#SBATCH --mem=12GB
+
+#SBATCH --mail-type=END
+#SBATCH --mail-user=michiel.vandermeer@student.uva.nl
+
+#Load the modules needed for your environment
+module load Python/3.6.3-foss-2017b
+module load CUDA/9.0.176
+
+# Go to machine directory
+pushd ${HOME}/machine/
+
+# Run training
+python3 train_rl.py \
+    --env-name BabyAI-TransferGoToObjBeforeAfter0-v0 \
+    --print_every 1 \
+    --slurm_id $SLURM_JOB_ID \
+    --tb \
+    --seed 1 \
+    --reasoning \
+    --reason_coef 2 \
+    --drop_diag \
+    --diag_targets 21 \
+    --load_checkpoint models/BabyAI-CustomGoToObjMedium-v0-_PPO_AC_expert_filmcnn_gru_mem_seed100_job2669862_19-07-16-14-16-39/014000_check.pt \
+    --resume
+
---
+++ jobs/transfer/beforeafter/diag_transfer2_base_beforeafter.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -p gpu_shared
+#SBATCH -t 1:00:00
+#SBATCH --mem=12GB
+
+#SBATCH --mail-type=END
+#SBATCH --mail-user=michiel.vandermeer@student.uva.nl
+
+#Load the modules needed for your environment
+module load Python/3.6.3-foss-2017b
+module load CUDA/9.0.176
+
+# Go to machine directory
+pushd ${HOME}/machine/
+
+# Run training
+python3 train_rl.py \
+    --env-name BabyAI-TransferGoToObjBeforeAfter2-v0 \
+    --print_every 1 \
+    --slurm_id $SLURM_JOB_ID \
+    --tb \
+    --seed 1 \
+    --reasoning \
+    --reason_coef 2 \
+    --drop_diag \
+    --diag_targets 28 \
+    --load_checkpoint models/BabyAI-CustomGoToObjMedium-v0-_PPO_AC_expert_filmcnn_gru_mem_seed1_job2669860_19-07-16-13-28-33/014000_check.pt \
+    --resume
+
