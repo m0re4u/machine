3c77a77a02175dd9e11fad118aee30670dffd74c
Michiel van der Meer <michiel@vandermeer.ai>
2019-07-31 10:47:13 +0200

    Correct level :sigh:

--- jobs/sparse_diag/sparse_diag_BA_42.sh
+++ jobs/sparse_diag/sparse_diag_BA_42.sh
@@ -16,7 +16,7 @@ pushd ${HOME}/machine/
 
 # Run training
 python3 train_rl.py \
-    --env-name BabyAI-CustomGoToObjBeforeAfter-v0 \
+    --env-name BabyAI-CustomGoToObjMedium-v0 \
     --print_every 1 \
     --slurm_id $SLURM_JOB_ID \
     --tb \
