ed66903e38c429f6ecdbc60c5937833b474c2394
Michiel van der Meer <michiel@vandermeer.ai>
2019-09-17 11:21:15 +0200

    repeat level jobs for transfer

---
+++ jobs/transfer/multiple/transfer0_base_multiple42.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -p gpu_shared
+#SBATCH -t 2:00:00
+#SBATCH --mem=12GB
+
+#SBATCH --mail-type=END
+#SBATCH --mail-user=michiel.vandermeer@student.uva.nl
+
+#Load the modules needed for your environment
+module load Python/3.6.3-foss-2017b
+module load CUDA/9.0.176
+
+# Go to machine directory
+pushd ${HOME}/machine/
+
+# Run training
+python3 train_rl.py \
+    --env-name BabyAI-TransferGoToObjMultiple0-v0 \
+    --print_every 1 \
+    --slurm_id $SLURM_JOB_ID \
+    --tb \
+    --seed 1 \
+    --load_checkpoint models/BabyAI-CustomGoToObjMultiple-v0-_PPO_AC_expert_filmcnn_gru_mem_seed42_job2818254_19-08-09-16-11-33/021900_check.pt \
+    --resume
+
---
+++ jobs/transfer/multiple/transfer1_new_multiple42.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -p gpu_shared
+#SBATCH -t 2:00:00
+#SBATCH --mem=12GB
+
+#SBATCH --mail-type=END
+#SBATCH --mail-user=michiel.vandermeer@student.uva.nl
+
+#Load the modules needed for your environment
+module load Python/3.6.3-foss-2017b
+module load CUDA/9.0.176
+
+# Go to machine directory
+pushd ${HOME}/machine/
+
+# Run training
+python3 train_rl.py \
+    --env-name BabyAI-TransferGoToObjMultiple1-v0 \
+    --print_every 1 \
+    --slurm_id $SLURM_JOB_ID \
+    --tb \
+    --seed 1 \
+    --reasoning \
+    --reason_coef 2 \
+    --drop_diag \
+    --diag_targets 24 \
+    --load_checkpoint models/BabyAI-CustomGoToObjMultiple-v0-_PPO_IAC_expert_filmcnn_gru_mem_seed42_job2812455_19-08-07-13-49-16/021900_check.pt \
+    --resume
+
